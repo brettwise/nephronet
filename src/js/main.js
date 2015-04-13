2b21b77270fdb72c64a0c414d80bd83c25a139f6
Brett Wise <brettdwise@gmail.com>
2015-04-13 19:20:42 -0400

    Final changes.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
-// this is a test
+(function ($) {
+  'use strict';
+  var $body    = $('html, body'), // Define jQuery collection 
+      content  = $('#main').smoothState({
+        prefetch: true,
+        pageCacheSize: 4,
+        onStart : {
+          duration: 250,
+          render: function () {
+            content.toggleAnimationClass('is-exiting');
+            
+            // Scroll user to the top
+            $body.animate({ 'scrollTop': 0 });
+
+          }
+        }
+      }).data('smoothState');
+})(jQuery);
