13a81a1379feb34d3db884be01cce64a72c6b84d
Brett Wise <brettdwise@gmail.com>
2016-02-23 17:16:37 -0500

    Update 2016-02-23T22:16:34.716Z

--- gulp/tasks/js.js
+++
@@ -1,9 +0,0 @@
-var gulp         = require('gulp');
-var browserSync  = require('browser-sync');
-var config       = require('../config').js;
-
-gulp.task('js', function () {
-  return gulp.src(config.src)
-    .pipe(gulp.dest(config.dest))
-    .pipe(browserSync.reload({stream: true}));
-});
